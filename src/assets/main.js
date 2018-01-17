7e756fc617093ed94d3dd10c156b9c6712294636
Giantcoder <mattjotoole@gmail.com>
2018-01-17 23:08:05 +0000

    Main.js - added divs for completed courses!  :thumbsup:

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
+  $.ajax({
+     url: 'https://www.codeschool.com/users/GiantCoder.json',
+     dataType: 'jsonp',
+     success: function(response) {
+       displayCourses(response.courses.completed);
+     }
+   });
 
-  // your code will go here
+const displayCourses = function (data) {
+  const badges = $('#badges');
+  data.forEach(function (course) {
+    let badge = '<div class="course">';
+    badge += '<h3>' + course.title + '</h3>';
+    badge += '<img src="' + course.badge + '"/>';
+    badge += '<a class="btn btn-primary" href="' + course.url + '" target="_blank">See Course</a>'
+    badge += '</div>';
+    badges.append(badge);
+  });
+}
 
 });
