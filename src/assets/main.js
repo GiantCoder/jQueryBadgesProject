bfa2cf4c7f83374da3e3a6db98f28eb42f948aa7
Giantcoder <mattjotoole@gmail.com>
2018-01-18 15:25:52 +0000

    npm install and modified main.js to append divs in a slightly different way :thinking:

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,14 +8,34 @@ $(function() {
    });
 
 const displayCourses = function (data) {
+
   const badges = $('#badges');
+
   data.forEach(function (course) {
-    let badge = '<div class="course">';
-    badge += '<h3>' + course.title + '</h3>';
-    badge += '<img src="' + course.badge + '"/>';
-    badge += '<a class="btn btn-primary" href="' + course.url + '" target="_blank">See Course</a>'
-    badge += '</div>';
-    badges.append(badge);
+    let $course = $('<div />', {
+      'class': 'course',
+    }).appendTo(badges);
+
+    $('<h3>', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img>', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      class: 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+
+    // let badge = '<div class="course">';
+    // badge += '<h3>' + course.title + '</h3>';
+    // badge += '<img src="' + course.badge + '"/>';
+    // badge += '<a class="btn btn-primary" href="' + course.url + '" target="_blank">See Course</a>'
+    // badge += '</div>';
   });
 }
 
